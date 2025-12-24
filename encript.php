6db3d3664d6598d6004093f55c56d2f33191e4bb
abii16 <danielfekede1616@gmail.com>
2025-12-24 07:24:07 +0300

    TOUR-13 Session / Token Management

--- encript.php
+++
@@ -1,9 +0,0 @@
-<?php
-$plainPassword = 'user_password123';
-$hashedPassword = password_hash($plainPassword, PASSWORD_DEFAULT);
-$loginPassword = 'user_password123';
-if (password_verify($loginPassword, $hashedPassword)) {
-    echo "Login successful!";
-} else {
-    echo "Invalid credentials.";
-}
